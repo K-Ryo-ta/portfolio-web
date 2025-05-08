4cfef4bff84f41980d79fed3a65dc13ab2f35cb5
K-Ryo-ta <studyprograming51@gmail.com>
2025-05-08 16:43:23 +0900

    内容を書き加えた。

--- data/socialLinkData.ts
+++ data/socialLinkData.ts
@@ -6,18 +6,18 @@ export type socialLinkData = {
 export const socialLinks: socialLinkData[] = [
   {
     name: "GitHub",
-    url: "https://github.com",
+    url: "https://github.com/K-Ryo-ta",
   },
   {
     name: "X",
-    url: "https://twitter.com",
+    url: "https://x.com/KR_programing",
   },
   {
     name: "Wantedly",
-    url: "https://linkedin.com",
+    url: "https://www.wantedly.com/id/ryouta_kotani",
   },
   {
     name: "Qiita",
-    url: "https://linkedin.com",
+    url: "https://qiita.com/Ryota_programing",
   },
 ];
